6d1bd6f9f910961c622a893bf699756c88727f2d
Paloma <casagrande.paloma@gmail.com>
2022-09-24 14:47:46 -0300

    [feat] (integration test) test route post /recommendations, fail case, recommendation already exist

--- back-end/tests/integration/app.test.ts
+++ back-end/tests/integration/app.test.ts
@@ -3,6 +3,7 @@ import app from "../../src/app";
 import { deleteAllData } from "./factories/scenarioFactory";
 import recommendationFactory from "./factories/recommendationFactory";
 import { prisma } from "../../src/database";
+import { Recommendation } from "@prisma/client";
 
 const server = supertest(app);
 
@@ -25,9 +26,37 @@ describe('Tests with recommendations', ()=>{
         expect(createdRecommendation).not.toBeNull();
 
     });
-    it.todo('tests with POST /recommendations, try to create a recommendation that already exist, return conflict code');
+    it('tests with POST /recommendations, try to create a recommendation that already exist, return conflict code',async () => {
+        const newRecommendation = recommendationFactory();
+
+        await server.post('/recommendations').send(newRecommendation);
+        const result = await server.post('/recommendations').send(newRecommendation);
+
+        expect(result.status).toBe(409);
+        
+    });
     
-    it.todo('tests with POST /recommendations/:id/upvote, creates a upvote to that recommendation with sucess');
+    it('tests with POST /recommendations/:id/upvote, creates a upvote to that recommendation with sucess',async () => {
+        const newRecommendation = recommendationFactory();
+
+        await server.post('/recommendations').send(newRecommendation);
+        
+        const createdRecommendation: Recommendation = await prisma.recommendation.findFirst({
+            where:{ name: newRecommendation.name}
+        });
+        
+        const id = createdRecommendation.id;
+        const result = await server.post(`/recommendations/${+id}/upvote`);
+
+        const uptadedRecommendation = await prisma.recommendation.findFirst({
+            where:{ name: newRecommendation.name}
+        });
+
+        expect(result.status).toBe(200);
+        expect(uptadedRecommendation.score).toEqual(createdRecommendation.score +1);
+
+
+    });
     it.todo('tests with POST /recommendations/:id/upvote, try to creates a upvote to an inexistent recommendation ');
 
     it.todo('tests with POST /recommendations/:id/downvote, creates a downvote to that recommendation with sucess');
